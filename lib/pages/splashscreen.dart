e63672414a4a5d21975e2f871ec929b8256063f8
Adipurnama7 <adipurnamaa8@gmail.com>
2024-11-14 08:08:30 +0700

    initial commit

---
+++ lib/pages/splashscreen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Simulate a delay for the splash screen
+    Future.delayed(Duration(seconds: 2), () {
+      // Navigate to the home screen after the delay
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background image
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/bg1.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          // Centered content
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset('images/logo.png'),
+                SizedBox(height: 20),
+                Text(
+                  'HalloTad',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
